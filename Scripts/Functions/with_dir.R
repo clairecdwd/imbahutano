2b274ffe00de96572853e15936084f8d76f2babc
Claire Calderwood <72615895+clairecdwd@users.noreply.github.com>
2024-07-12 17:45:12 +0200

    Delete Scripts directory

--- Scripts/Functions/with_dir.R
+++
@@ -1,7 +0,0 @@
-# Change directory to do something and then go back to the previous one
-with_dir <- function(dir, expr) {
-    old_wd <- getwd()
-    on.exit(setwd(old_wd))
-    setwd(dir)
-    evalq(expr)
-}
